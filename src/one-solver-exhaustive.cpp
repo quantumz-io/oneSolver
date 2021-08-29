a73bd611b0961f227fd41f507fdcf4cd169fa9c6
Manish Gupta <gupta@camk.edu.pl>
2021-08-29 00:31:15 +0000

    one to one serialized communication Update one-solver-exhaustive.cpp

--- src/one-solver-exhaustive.cpp
+++ src/one-solver-exhaustive.cpp
@@ -4,11 +4,17 @@
  *
  */
 
+// include headers that implement a archive in simple text format
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 #include <boost/program_options.hpp>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <ctime>
+#include <vector>
 
+#include <mpi.h>
 #include "CL/sycl.hpp"
 #include "helpers/devices.hpp"
 #include "exhaustive/exhaustive.hpp"
@@ -16,73 +22,17 @@
 #include "model/qubo.hpp"
 #include "model/solution.hpp"
 
+#define MAX_BUFFER_SIZE 12000
+
 namespace po = boost::program_options;
 namespace sycl = cl::sycl;
 
 using queue_ptr = std::unique_ptr<sycl::queue>;
 
-int main(int argc, char *argv[]) {
-  const clock_t begin_time = clock();
 
-  std::string input_file;
-  std::string output_file;
-  std::string device_type;
-
-  try {
-
-    po::options_description options("Allowed options");
-    options.add_options()("help", "produce help message")(
-        "input", po::value<std::string>(&input_file), "input file")(
-        "output", po::value<std::string>(&output_file), "output file")(
-        "device-type",
-        po::value<std::string>(&device_type)->default_value("host"),
-        "device type to use (cpu, gpu or host)");
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, options), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-      std::cout << options << std::endl;
-      return 0;
-    }
-
-    if (!vm.count("input")) {
-      std::cerr << "No input file provided." << std::endl;
-      return -1;
-    }
-
-    if (!vm.count("output")) {
-      std::cerr << "No output file provided." << std::endl;
-      return -1;
-    }
-
-    if (device_type != "cpu" && device_type != "gpu" && device_type != "host") {
-      std::cerr << "Unknown device type: " << device_type << std::endl;
-      return -1;
-    }
-
-    std::cout << "Reading input from: " << input_file << std::endl;
-
-    std::cout << "Output will be saved to: " << output_file << std::endl;
-
-    std::ifstream qubo_file(input_file);
-
-    if (!qubo_file) {
-      std::cerr << "can not open input file: " << input_file << std::endl;
-      return -1;
-    }
-    
-    // Start MPI.
-    if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
-      std::cout << "Failed to initialize MPI\n";
-      exit(-1);
-      }
-
-    qubo_file.unsetf(std::ios::skipws);
-    auto instance = qubo::QUBOModel<int, double>::load(qubo_file);
-
-    queue_ptr q_ptr;
+qubo::Solution sycl_native(qubo::QUBOModel<int, double> instance, std::string device_type)
+{
+  queue_ptr q_ptr;
 
     try {
       q_ptr = queue_ptr(
@@ -98,11 +48,188 @@ int main(int argc, char *argv[]) {
 
     auto solution = exhaustive::solve(*q_ptr, instance);
 
-    std::ofstream results_file(output_file);
+    return solution;
 
-    solution.save(results_file);
+}
 
-    results_file.close();
+int main(int argc, char *argv[]) {
+  const clock_t begin_time = clock();
+
+  char machine_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len=0;
+  int rank=0;
+  int num_procs=0;
+  int size=0;
+  int count =0;
+
+  std::string input_file;
+  std::string output_file;
+  std::string device_type;
+
+  MPI::Status msgStatus;
+
+  try {
+
+    //MPI::Init(argc, argv);
+    // Start MPI.
+    if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
+      std::cout << "Failed to initialize MPI\n";
+      exit(-1);
+    }
+
+    // Create the communicator, and retrieve the number of MPI ranks.
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    // Determine the rank number.
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Get the machine name.
+    MPI_Get_processor_name(machine_name, &name_len);
+
+    std::cout << "Rank #" << rank << " runs on: " << machine_name;
+              // << ", uses device: "
+              // << myQueue.get_device().get_info<info::device::name>() << "\n";
+
+    if(rank == 0){
+      po::options_description options("Allowed options");
+      options.add_options()("help", "produce help message")(
+        "input", po::value<std::string>(&input_file), "input file")(
+        "output", po::value<std::string>(&output_file), "output file")(
+        "device-type",
+        po::value<std::string>(&device_type)->default_value("host"),
+        "device type to use (cpu, gpu or host)");
+
+      po::variables_map vm;
+      po::store(po::parse_command_line(argc, argv, options), vm);
+      po::notify(vm);
+
+      if (vm.count("help")) {
+        std::cout << options << std::endl;
+        MPI_Finalize();
+        return 0;
+      }
+
+      if (!vm.count("input")) {
+        std::cerr << "No input file provided." << std::endl;
+        MPI_Finalize();
+        return -1;
+      }
+
+      if (!vm.count("output")) {
+        std::cerr << "No output file provided." << std::endl;
+        MPI_Finalize();
+        return -1;
+      }
+
+      if (device_type != "cpu" && device_type != "gpu" && device_type != "host") {
+        std::cerr << "Unknown device type: " << device_type << std::endl;
+        MPI_Finalize();
+        return -1;
+      }
+
+      std::cout << "Reading input from: " << input_file << std::endl;
+
+      std::cout << "Output will be saved to: " << output_file << std::endl;
+
+      std::ifstream qubo_file(input_file);
+
+      if (!qubo_file) {
+        std::cerr << "can not open input file: " << input_file << std::endl;
+        MPI_Finalize();
+        return -1;
+      }
+
+      qubo_file.unsetf(std::ios::skipws);
+      auto instance = qubo::QUBOModel<int, double>::load(qubo_file);
+
+      MPI::COMM_WORLD.Send(device_type.c_str(),
+                           device_type.size(),
+                           MPI::Datatype(MPI::CHAR),
+                           1,
+                           1);
+
+      // MPI::COMM_WORLD.Bcast((void*)device_type.c_str(),
+      //                      device_type.size(),
+      //                      MPI::Datatype(MPI::CHAR),
+      //                      rank);
+
+      //std::cout << "tSize: " << device_type.size() << "\n";
+      std::cout << "device_type: " << device_type.c_str() << "\n";
+
+      std::ostringstream oss;
+      
+      // save data to archive
+      {
+          boost::archive::text_oarchive ar(oss);
+          // write class instance to archive
+          ar << instance;
+    	  // archive and stream closed when destructors are called
+      }
+
+      //std::cout << "tSize: " << oss.str().size() << "\n";
+      //std::cout << "tData: " << oss.str().c_str() << "\n";
+
+
+      //MPI_Send(stream.str().c_str(), stream.str().size(), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
+
+
+      MPI::COMM_WORLD.Send(oss.str().c_str(),
+                           oss.str().size(),
+                           MPI::Datatype(MPI_BYTE),
+                           1,
+                           1);
+
+      auto solution = sycl_native(instance, device_type);
+
+      std::ofstream results_file(output_file);
+
+      solution.save(results_file);
+
+      results_file.close();
+
+    }
+    else{
+
+       std::vector<char> incomingBuffer(MAX_BUFFER_SIZE);
+       
+       MPI::COMM_WORLD.Recv(&incomingBuffer[0], incomingBuffer.size(),
+                            MPI::Datatype(MPI::CHAR),
+                            0,
+                            1, msgStatus);
+       count = msgStatus.Get_count(MPI::Datatype(MPI_CHAR));
+       if (count > 0)
+       {
+         device_type = std::string(&incomingBuffer[0], count);
+       }
+       
+       MPI::COMM_WORLD.Recv(&incomingBuffer[0], incomingBuffer.size(),
+                            MPI::Datatype(MPI_BYTE),
+                            0,
+                            1, msgStatus);
+
+       incomingBuffer.resize(msgStatus.Get_count(MPI::Datatype(MPI_BYTE)));
+
+       if (incomingBuffer.size() > 0) {
+         std::istringstream iss(std::string(&incomingBuffer[0], incomingBuffer.size()));
+         //std::cout << "rSize: " << iss.str().size() << std::endl;
+         //std::cout << "rData: " << iss.str().c_str() << std::endl;
+         
+         qubo::QUBOModel<int, double> instance;
+         {
+           boost::archive::text_iarchive ar(iss);   
+           ar >> instance;
+         }
+    
+        auto solution = sycl_native(instance, device_type);
+
+       }
+
+      
+    }
+    
+
   } catch (std::exception &e) {
     std::cerr << "error: " << e.what() << "\n";
     return 1;
