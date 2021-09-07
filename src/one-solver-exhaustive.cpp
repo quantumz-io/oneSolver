549f973164f757fd7456a8ee32d943aec36dc556
Manish Gupta <gupta@camk.edu.pl>
2021-09-07 15:16:48 +0000

    functional version of one-solver-exhaustive.cpp with MPI.

--- src/one-solver-exhaustive.cpp
+++ src/one-solver-exhaustive.cpp
@@ -22,7 +22,6 @@
 #include "model/qubo.hpp"
 #include "model/solution.hpp"
 
-#define MAX_BUFFER_SIZE 12000
 
 namespace po = boost::program_options;
 namespace sycl = cl::sycl;
@@ -30,10 +29,10 @@ namespace sycl = cl::sycl;
 using queue_ptr = std::unique_ptr<sycl::queue>;
 
 
-qubo::Solution sycl_native(qubo::QUBOModel<int, double> instance, std::string device_type)
+qubo::Solution sycl_native(qubo::QUBOModel<int, double> instance, std::string device_type, ulong start_state, ulong end_state)
 {
   queue_ptr q_ptr;
-
+  
     try {
       q_ptr = queue_ptr(
           new sycl::queue(*devices::construct_device_selector(device_type)));
@@ -46,7 +45,7 @@ qubo::Solution sycl_native(qubo::QUBOModel<int, double> instance, std::string de
               << q_ptr->get_device().get_info<sycl::info::device::name>()
               << std::endl;
 
-    auto solution = exhaustive::solve(*q_ptr, instance);
+    auto solution = exhaustive::solve(*q_ptr, instance, start_state, end_state);
 
     return solution;
 
@@ -58,17 +57,27 @@ int main(int argc, char *argv[]) {
   char machine_name[MPI_MAX_PROCESSOR_NAME];
   int name_len=0;
   int rank=0;
+  int process_rank = 0;
   int num_procs=0;
   int size=0;
   int count =0;
   long long int msg_size =0;
 
+  ulong start_state = 0;
+  ulong end_state = 0;
+
+  ulong *ranges_buf = NULL;
+  ulong recv_arr[2];
+
   std::string input_file;
   std::string output_file;
   std::string device_type;
   std::vector<char> msg_buff;
+  
 
-  MPI::Status msg_status;
+  
+
+  //MPI::Status msg_status;
 
   qubo::QUBOModel<int, double> instance;
   
@@ -217,15 +226,111 @@ int main(int argc, char *argv[]) {
       //std::cout << "device_type: " << device_type << std::endl;
     }
 
-    auto solution = sycl_native(instance, device_type);
+    if(rank == 0){
+      // Divide task to each node and let sycl decide the distribution of task on each node.
+      auto n_bits = instance.get_nodes();
+      //std::vector<std::vector<ulong>> ranges_buf(num_procs, std::vector<ulong> (2,0));
+      //ulong ** ranges_buf = new ulong[num_procs][2];
+      //auto ranges_buf = new ulong [num_procs][2];
+      ranges_buf = new ulong[num_procs*2];
+      // note that all the variables related to states have to be unsigned 64 bit integers
+      ulong n_states = 1 << n_bits; 
+      ulong states_per_node = n_states / num_procs;
+      ulong states_per_node_remainder = n_states % num_procs;
 
-    std::cout << "Solution: " << solution.energy << std::endl;
+      ulong reminder_count = 0;
+      ulong states_count = 0;
+
+      for (auto i = 0; i < num_procs; ++i) {
+        start_state = states_count;
+        states_count += states_per_node;
+        if (reminder_count < states_per_node_remainder) {
+          states_count += 1;
+          reminder_count++;
+        }
+        end_state = states_count;
+
+      ranges_buf[i*2] = start_state;
+      ranges_buf[i*2+1] = end_state;
+#ifdef DEBUG
+      std::cout << ranges_buf[i*2];
+      std::cout << ", ";
+      std::cout << ranges_buf[i*2+1] << std::endl;
+#endif
+      }
+      // ulong* p =  &ranges_buf[0];
+      // for (auto i = 0; i < num_procs*2; ++i) {
+       
+      //  std::cout << p << ":" << *p << std::endl;
+      //  ++p;
+      //  //std::cout << p << ":" << *p << std::endl;
+      // }
+    }
+
+    MPI::COMM_WORLD.Scatter(ranges_buf, 2, MPI::UNSIGNED_LONG,recv_arr, 2, MPI::UNSIGNED_LONG, 0);
+    //std::cout << "Rank #" << rank << " range: " << recv_arr[0] << "-" << recv_arr[1] << std::endl;
+    start_state = recv_arr[0];
+    end_state = recv_arr[1];
+
+
+    auto solution = sycl_native(instance, device_type, start_state, end_state);
+
+    //std::cout << "Solution: " << solution.energy << std::endl;
+
+    
+    double *energy_buff = new double[num_procs];
+
+    MPI::COMM_WORLD.Gather(&solution.energy, 1, MPI::DOUBLE, energy_buff, 1, MPI::DOUBLE, 0);
+    
+    if(rank == 0){
+       std::vector<double> energies(&energy_buff[0], &energy_buff[num_procs]);
+       auto min_energy = std::min_element(energies.begin(), energies.end());
+       auto min_idx_energy = std::distance(energies.begin(), min_energy);
+       //std::cout << "min_idx: " << energies[min_idx_energy] << std::endl;
+       solution.energy = energies[min_idx_energy];
+
+       process_rank = min_idx_energy;
+       //std::cout << min_energy << std::endl;
+       
+      //  for (auto i = energies.begin(); i != energies.end(); ++i)
+      //     std::cout << *i << ' ';
+
+      //  for (auto i = 0; i < num_procs; ++i) {
+      //  std::cout << "energy: " << ":" <<  energy_buff[i] << std::endl;
+      //  }
+    }
+
+    MPI::COMM_WORLD.Bcast(&process_rank, 1, MPI::INT, 0);
+
+    
+    if ((process_rank !=0) && (rank == process_rank))
+    {
+       auto state = solution.state; 
+       MPI::COMM_WORLD.Send(state.data(), state.size(), MPI_CHAR, 0, 0);
+       //std::cout << "rank: " << rank << std::endl;
+    }
 
     if(rank == 0){
+      char buff[32];
+      if(process_rank != 0){
+        MPI::COMM_WORLD.Recv(&buff, 32, MPI_CHAR, process_rank, 0 );
+        // for (auto i = 0; i < solution.state.size(); ++i) {
+        // std::cout << (int)buff[i] << " ";
+        // }
+        // std::cout << std::endl;
+        for(auto i=0; i < solution.state.size(); ++i ){
+          solution.state[i] = buff[i];
+        } 
+      }
+      // for (auto i = 0; i < solution.state.size(); ++i) {
+      //   std::cout << (int)solution.state[i] << " ";
+      // }
+      std::cout << std::endl;
       std::ofstream results_file(output_file);
       solution.save(results_file);
       results_file.close();
     }
+    
 
     
   } catch (std::exception &e) {
@@ -235,6 +340,7 @@ int main(int argc, char *argv[]) {
     std::cerr << "Exception of unknown type!\n";
   }
   if(rank == 0){
+    delete ranges_buf;
     std::cout << "Calculation time [s]: "<< float( clock () - begin_time ) /  CLOCKS_PER_SEC << "\n";
   }
   
