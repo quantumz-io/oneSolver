14f147449047d938ae0bac51956be72cc2e36104
Manish Kumar Gupta <gupta@camk.edu.pl>
2021-09-23 13:36:17 +0200

    Added error handling and seperated cmd line parsing.

--- src/one-solver-anneal.cpp
+++ src/one-solver-anneal.cpp
@@ -43,19 +43,23 @@ void construct_geometric_beta_schedule(std::vector<double> &schedule,
   }
 }
 
-qubo::Solution sycl_native_anneal(qubo::QUBOModel<int, double> instance, std::string device_type, std::string schedule_type, std::uint64_t seed, double beta_min, double beta_max, uint num_iter, uint num_tries)
+/*!
+ * @brief Compute QUBO solution in a give solution space on a given device.
+ * @param Instance of the proble to solve.
+ * @param Accelerator device type.
+ * @param beta schedule type linear or geometric.
+ * @param seed for random number generator.
+ * @param minimum value of beta in the annealing schedule.
+ * @param maximum value of beta in the annealing schedule.
+ * @param number of iterations of the algorithm.
+ * @param number of trajectories to try.
+ *
+ * @returns solution to the given QUBO problem and acclerator device name.
+ */
+std::tuple<qubo::Solution, std::string> sycl_native_anneal(qubo::QUBOModel<int, double> instance, std::string device_type, std::string schedule_type, std::uint64_t seed, double beta_min, double beta_max, uint num_iter, uint num_tries)
 {
   queue_ptr q_ptr;
-
-  char machine_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len=0;
-  int rank = 0;
-    
-  // Determine the rank number.
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  // Get the machine name.
-  MPI_Get_processor_name(machine_name, &name_len);
+  std::string accl_device_name;
 
   try {
     q_ptr = queue_ptr(
@@ -65,9 +69,10 @@ qubo::Solution sycl_native_anneal(qubo::QUBOModel<int, double> instance, std::st
               << std::endl;
   }
   
-  std::cout << "Node ID [" << machine_name << "], " << "Rank [" << rank << "], " << "Using device: "
-            << q_ptr->get_device().get_info<sycl::info::device::name>()
-            << std::endl;
+  accl_device_name = q_ptr->get_device().get_info<sycl::info::device::name>();
+  // std::cout << "Node ID [" << machine_name << "], " << "Rank [" << rank << "], " << "Using device: "
+  //           << q_ptr->get_device().get_info<sycl::info::device::name>()
+  //           << std::endl;
 
   std::vector<double> beta_schedule(num_iter);
 
@@ -82,7 +87,81 @@ qubo::Solution sycl_native_anneal(qubo::QUBOModel<int, double> instance, std::st
   auto solution =
       sa::anneal(instance, *q_ptr, beta_schedule, seed, num_iter, num_tries);
 
-  return solution;
+  return {solution, accl_device_name};
+}
+
+/*!
+ * @brief Parse command line.
+ * @param argument count.
+ * @param arguments array.
+ *
+ * @returns returns input_file, output_file, device_type, beta_min, beta_max, num_iter and num_tries.
+ */
+std::tuple<std::string, std::string, std::string, std::string, double, double, uint, uint> parse_cmd_line_anneal(int argc, char *argv[])
+{
+  std::string input_file;
+  std::string output_file;
+  std::string schedule_type;
+  std::string device_type;
+
+  uint num_iter;
+  uint num_tries;
+
+  double beta_min, beta_max;
+
+  po::options_description options("Allowed options");
+  options.add_options()("help", "produce help message")(
+     "input", po::value<std::string>(&input_file), "input file")(
+     "output", po::value<std::string>(&output_file), "output file")(
+     "num-iter", po::value<uint>(&num_iter)->default_value(100),
+     "number of iterations of the algorithm")(
+     "num-tries", po::value<uint>(&num_tries)->default_value(100),
+     "number of trajectories to try")(
+     "schedule-type",
+     po::value<std::string>(&schedule_type)->default_value("geometric"),
+     "type of beta schedule to use, either linear or geometric")(
+     "beta-min", po::value<double>(&beta_min)->default_value(0.1),
+     "minimum value of beta in the annealing schedule (default 0.1)")(
+     "beta-max", po::value<double>(&beta_max)->default_value(1.0),
+     "maximum value of beta in the annealing schedule (default 1.0)")(
+     "device-type",
+     po::value<std::string>(&device_type)->default_value("host"),
+     "device type to use (cpu, gpu or host)");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, options), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << options << std::endl;
+    MPI_Abort(MPI_COMM_WORLD, MPI_SUCCESS);
+  }
+
+  if (!vm.count("input")) {
+    throw std::runtime_error("No input file provided.");
+  }
+
+  if (!vm.count("output")) {
+    throw std::runtime_error("No output file provided.");
+  }
+
+  if (device_type != "cpu" && device_type != "gpu" && device_type != "host") {
+    throw std::runtime_error("Unknown device type: " + device_type);
+  }
+
+  if (schedule_type != "linear" && schedule_type != "geometric") {
+    throw std::runtime_error("Unknown beta schedule: " + schedule_type);
+  }
+
+  if (beta_max < 0 || beta_min < 0) {
+    throw std::runtime_error("Invalid schedule, both ends of beta range need to be positive.");
+  }
+
+  if (beta_min >= beta_max) {
+    throw std::runtime_error("Invalid schedule, initial beta is not lesser than final beta.");
+  }
+  
+  return {input_file, output_file, device_type, schedule_type, beta_min, beta_max, num_iter, num_tries};
 }
 
 int main(int argc, char *argv[]) {
@@ -91,8 +170,8 @@ int main(int argc, char *argv[]) {
   char machine_name[MPI_MAX_PROCESSOR_NAME];
   int name_len = 0;
   int rank = 0;
-  int root = 0; // Rank zero process
-  int process_rank = 0;
+  const int root_rank = 0; // Rank zero process.
+  int min_energy_process_rank = 0; // Rank of process with minimum energy.
   int num_procs = 0;
   int size = 0;
   
@@ -127,77 +206,12 @@ int main(int argc, char *argv[]) {
     // Determine the rank number.
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
-#ifdef DEBUG
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
     // Get the machine name.
     MPI_Get_processor_name(machine_name, &name_len);
 
-    std::cout << " Node ID: " << machine_name << " Rank #" << rank;
-#endif  
-
-    if (rank == 0) {
-      po::options_description options("Allowed options");
-      options.add_options()("help", "produce help message")(
-         "input", po::value<std::string>(&input_file), "input file")(
-         "output", po::value<std::string>(&output_file), "output file")(
-         "num-iter", po::value<uint>(&num_iter)->default_value(100),
-         "number of iterations of the algorithm")(
-         "num-tries", po::value<uint>(&num_tries)->default_value(100),
-         "number of trajectories to try")(
-         "schedule-type",
-         po::value<std::string>(&schedule_type)->default_value("geometric"),
-         "type of beta schedule tu use, either linear or geometric")(
-         "beta-min", po::value<double>(&beta_min)->default_value(0.1),
-         "minimum value of beta in the annealing schedule (default 0.1)")(
-         "beta-max", po::value<double>(&beta_max)->default_value(1.0),
-         "maximum value of beta in the annealing schedule (default 1.0)")(
-         "device-type",
-         po::value<std::string>(&device_type)->default_value("host"),
-         "device type to use (cpu, gpu or host)");
-
-      po::variables_map vm;
-      po::store(po::parse_command_line(argc, argv, options), vm);
-      po::notify(vm);
-
-      if (vm.count("help")) {
-        std::cout << options << std::endl;
-        return 0;
-      }
-
-      if (!vm.count("input")) {
-        std::cerr << "No input file provided." << std::endl;
-        return -1;
-      }
-
-      if (!vm.count("output")) {
-        std::cerr << "No output file provided." << std::endl;
-        return -1;
-      }
-
-      if (device_type != "cpu" && device_type != "gpu" && device_type != "host") {
-        std::cerr << "Unknown device type: " << device_type << std::endl;
-        return -1;
-      }
-
-      if (schedule_type != "linear" && schedule_type != "geometric") {
-        std::cerr << "Unknown beta schedule: " << schedule_type << std::endl;
-        return -1;
-      }
-
-      if (beta_max < 0 || beta_min < 0) {
-        std::cerr
-            << "Invalid schedule, both ends of beta range need to be positive"
-            << std::endl;
-        return -1;
-      }
-
-      if (beta_min >= beta_max) {
-        std::cerr
-            << "Invalid schedule, initial beta is not lesser than final beta"
-            << std::endl;
-        return -1;
-      }
+    if (rank == root_rank) {
+      // Parse command line to get the parameters.
+      std::tie(input_file, output_file, device_type, schedule_type, beta_min, beta_max, num_iter, num_tries) = parse_cmd_line_anneal(argc, argv);
 
       std::cout << "Reading input from: " << input_file << std::endl;
 
@@ -238,19 +252,19 @@ int main(int argc, char *argv[]) {
     }
 
     // Send QUBOModel instance to all the proecesses.
-    if (MPI_Bcast(&msg_size, 1, MPI_LONG_LONG_INT, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(&msg_size, 1, MPI_LONG_LONG_INT, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     } 
   
-    if (rank != 0) {
+    if (rank != root_rank) {
       msg_buff.resize(msg_size);
     }
 
-    if (MPI_Bcast(msg_buff.data(), msg_size, MPI_CHAR, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(msg_buff.data(), msg_size, MPI_CHAR, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
     
-    if (rank != 0) {
+    if (rank != root_rank) {
       if (msg_buff.size() > 0) {
         std::istringstream iss(std::string(msg_buff.data(), msg_buff.size()));
 #ifdef DEBUG
@@ -265,24 +279,24 @@ int main(int argc, char *argv[]) {
     }
 
     // Send device_type to all the processes.
-    if (rank == 0) {
+    if (rank == root_rank) {
       msg_size = device_type.size();
       msg_buff.assign(device_type.c_str(),device_type.c_str() + device_type.size() +1);
     }
   
-    if (MPI_Bcast(&msg_size, 1, MPI_LONG_LONG_INT, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(&msg_size, 1, MPI_LONG_LONG_INT, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
 
-    if (rank != 0) {
+    if (rank != root_rank) {
       msg_buff.resize(msg_size);
     }
   
-    if (MPI_Bcast(msg_buff.data(), msg_size, MPI_CHAR, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(msg_buff.data(), msg_size, MPI_CHAR, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
 
-    if (rank != 0) {
+    if (rank != root_rank) {
       device_type = std::string(&msg_buff[0], msg_size);
 #ifdef DEBUG
       std::cout << "device_type: " << device_type << std::endl;
@@ -290,24 +304,24 @@ int main(int argc, char *argv[]) {
     }
 
     // Send schedule_type to all the processes.
-    if (rank == 0) {
+    if (rank == root_rank) {
       msg_size = schedule_type.size();
       msg_buff.assign(schedule_type.c_str(),schedule_type.c_str() + schedule_type.size() +1);
     }
   
-    if (MPI_Bcast(&msg_size, 1, MPI_LONG_LONG_INT, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(&msg_size, 1, MPI_LONG_LONG_INT, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
 
-    if (rank != 0) {
+    if (rank != root_rank) {
       msg_buff.resize(msg_size);
     }
   
-    if (MPI_Bcast(msg_buff.data(), msg_size, MPI_CHAR, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(msg_buff.data(), msg_size, MPI_CHAR, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
 
-    if (rank != 0) {
+    if (rank != root_rank) {
       schedule_type = std::string(&msg_buff[0], msg_size);
 #ifdef DEBUG
       std::cout << "schedule_type: " << schedule_type << std::endl;
@@ -315,16 +329,16 @@ int main(int argc, char *argv[]) {
     }
 
     // Send beta_min and beta_max to all the processes.
-    if(rank == 0){
+    if(rank == root_rank){
       beta_buff[0] = beta_min;
       beta_buff[1] = beta_max;
     }
 
-    if (MPI_Bcast(&beta_buff, 2, MPI::DOUBLE, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(&beta_buff, 2, MPI::DOUBLE, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
 
-    if (rank !=0) {
+    if (rank != root_rank) {
       beta_min = beta_buff[0];
       beta_max = beta_buff[1];
 #ifdef DEBUG
@@ -334,16 +348,16 @@ int main(int argc, char *argv[]) {
     }
 
     // Send num_iter and num_tries to all the processes.
-    if (rank == 0) {
+    if (rank == root_rank) {
       param_buff[0] = num_iter;
       param_buff[1] = num_tries;
     }
 
-    if (MPI_Bcast(&param_buff, 2, MPI_UNSIGNED, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(&param_buff, 2, MPI_UNSIGNED, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
 
-    if (rank !=0) {
+    if (rank != root_rank) {
       num_iter = param_buff[0];
       num_tries = param_buff[1];
 #ifdef DEBUG
@@ -353,7 +367,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Send the seed for random number generator to all the processes.
-    if (rank == 0) {
+    if (rank == root_rank) {
       seed_buff = new std::uint64_t[num_procs];
       // Seed generation using mt19937_64 i.e 64 bit Mersenne Twister by Matsumoto, 2000.
       //std::random_device rd; // for non-deterministic generator uncomment this line and
@@ -367,11 +381,11 @@ int main(int argc, char *argv[]) {
       }    
     }
     
-    if (MPI_Scatter(seed_buff, 1, MPI_INT64_T, &seed, 1, MPI_INT64_T, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Scatter(seed_buff, 1, MPI_INT64_T, &seed, 1, MPI_INT64_T, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI scatter failed\n");
     }
     
-    if (rank == 0) {
+    if (rank == root_rank) {
       delete [] seed_buff;
     }
 
@@ -379,15 +393,40 @@ int main(int argc, char *argv[]) {
     std::cout << "seed: " << seed << std::endl;
 #endif
 
-    auto solution = sycl_native_anneal(instance, device_type, schedule_type, seed, beta_min, beta_max, num_iter, num_tries);
+    auto [solution, accl_device_name] = sycl_native_anneal(instance, device_type, schedule_type, seed, beta_min, beta_max, num_iter, num_tries);
+
+    if(rank == root_rank){
+      //std::unique_ptr<char> log_buff_ptr(new char[100]);
+      MPI_Status status;
+      int msg_length;
+      
+      //char log_buff_ptr[100];
+      std::cout << "Node ID [" << machine_name << "], " << "Rank [" << rank << "], " << "Using device: " << accl_device_name << std::endl;
+      for(auto rank_indx = 1; rank_indx < num_procs; ++rank_indx){
+        MPI_Probe(rank_indx, 1, MPI_COMM_WORLD, &status);
+        MPI_Get_count(&status, MPI_CHAR, &msg_length);
+
+        std::unique_ptr<char> log_buff_ptr(new char[msg_length]{});
+        std::cout << log_buff_ptr.get();
+        //std::cout << "rank_indx: " << rank_indx << std::endl;
+        MPI_Recv(log_buff_ptr.get(), msg_length, MPI_CHAR, rank_indx, 1 , MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::string log_msg(log_buff_ptr.get());
+        //std::cout << log_buff_ptr.get() << std::endl;
+        std::cout << log_msg;
+      }
+    }else{
+      std::ostringstream log_stream;
+      log_stream << "Node ID [" << machine_name << "], " << "Rank [" << rank << "], " << "Using device: " << accl_device_name << std::endl;
+      MPI_Send(log_stream.str().c_str(), log_stream.str().size(), MPI_CHAR, root_rank, 1, MPI_COMM_WORLD);
+    }
 
     double *energy_buff = new double[num_procs];
 
-    if (MPI_Gather(&solution.energy, 1, MPI_DOUBLE, energy_buff, 1, MPI_DOUBLE, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Gather(&solution.energy, 1, MPI_DOUBLE, energy_buff, 1, MPI_DOUBLE, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI gather failed\n");
     }
 
-    if (rank == 0) {
+    if (rank == root_rank) {
        std::vector<double> energies(&energy_buff[0], &energy_buff[num_procs]);
        auto min_energy = std::min_element(energies.begin(), energies.end());
        auto min_idx_energy = std::distance(energies.begin(), min_energy);
@@ -396,24 +435,24 @@ int main(int argc, char *argv[]) {
 #endif
        solution.energy = energies[min_idx_energy];
 
-       process_rank = min_idx_energy;
+       min_energy_process_rank = min_idx_energy;
     }
 
-    if (MPI_Bcast(&process_rank, 1, MPI_INT, root, MPI_COMM_WORLD) != MPI_SUCCESS) {
+    if (MPI_Bcast(&min_energy_process_rank, 1, MPI_INT, root_rank, MPI_COMM_WORLD) != MPI_SUCCESS) {
       throw std::runtime_error("MPI broadcast failed\n");
     }
 
-    if ((process_rank != 0) && (rank == process_rank)) {
+    if ((min_energy_process_rank != root_rank) && (rank == min_energy_process_rank)) {
        auto state = solution.state; 
-       if (MPI_Send(state.data(), state.size(), MPI_CHAR, root, 0, MPI_COMM_WORLD) != MPI_SUCCESS) {
+       if (MPI_Send(state.data(), state.size(), MPI_CHAR, root_rank, 0, MPI_COMM_WORLD) != MPI_SUCCESS) {
          throw std::runtime_error("MPI send failed\n");
         }
     }
 
-    if (rank == 0) {
+    if (rank == root_rank) {
       char buff[32];
-      if (process_rank != 0) {
-        if (MPI_Recv(&buff, 32, MPI_CHAR, process_rank, root, MPI_COMM_WORLD, MPI_STATUS_IGNORE) != MPI_SUCCESS) {
+      if (min_energy_process_rank != 0) {
+        if (MPI_Recv(&buff, 32, MPI_CHAR, min_energy_process_rank, root_rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE) != MPI_SUCCESS) {
          throw std::runtime_error("MPI receive failed\n");
         }
 #ifdef DEBUG
@@ -444,12 +483,12 @@ int main(int argc, char *argv[]) {
 
   } catch (std::exception &e) {
     std::cerr << "error: " << e.what() << "\n";
-    return 1;
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
   } catch (...) {
     std::cerr << "Exception of unknown type!\n";
   }
 
-  if (rank == 0) {
+  if (rank == root_rank) {
     std::cout << "Calculation time [s]: "<< float( clock () - begin_time ) /  CLOCKS_PER_SEC << "\n";
   }
 
