960b3a37ea4a7ffe741741e8a886d425218af8f3
u87561 <u87561@s001-n059.aidevcloud>
2021-09-24 07:39:48 -0700

    Added scripts for pbs batch mode

---
+++ utils/devcloud/pbs/mpirunexh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+infile=benchmarks/exhaustive_search/examples/csp13.qubo
+outfile=csp13.csv
+MPIPROC=4
+WITHGPU=false
+
+if [ "$WITHGPU" = true ]; then
+    mpirun -n $MPIPROC ./build/src/one-solver-exhaustive --device-type gpu --input $infile --output $outfile
+elif [ "$WITHGPU" = false ]; then
+    mpirun -n $MPIPROC ./build/src/one-solver-exhaustive --input $infile --output $outfile
+else
+    echo "Unknown device option"
+fi
